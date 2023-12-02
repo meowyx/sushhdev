e40ccca267c02503f35be6723fb442f13cc5a343
meowyx <qafka256@gmail.com>
2023-12-03 00:46:14 +0600

    first commit

---
+++ components/mdx-components.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { useMDXComponent } from "next-contentlayer/hooks";
+import { Code } from "bright";
+
+Code.theme = {
+  dark: "github-dark",
+  light: "github-light",
+  lightSelector: "html.light",
+};
+
+const components = {
+  Image,
+  pre: Code,
+};
+
+interface MdxProps {
+  code: string;
+}
+
+export function Mdx({ code }: MdxProps) {
+  const Component = useMDXComponent(code);
+
+  return <Component components={components} />;
+}
